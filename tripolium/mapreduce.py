a14694225499b2b11ee350b0bd9904773502a469
Ryan Balfanz <ryan@ryanbalfanz.net>
2010-08-21 20:46:24 -0700

    Adding logging, should have commited this earlier, older commits probably will not compile

--- tripolium/mapreduce.py
+++ tripolium/mapreduce.py
@@ -9,6 +9,8 @@ Copyright (c) 2010 __MyCompanyName__. All rights reserved.
 A Python package for creating SQL/MR functions to be used with Aster Data's nCluster.
 """
 
+import logging
+import logging.handlers
 import random
 import sys
 try:
@@ -20,6 +22,29 @@ DEBUG = True
 
 COL_DELIMITER = '\t'
 ROW_DELIMITER = '\n'
+if DEBUG:
+	LOG_FILENAME = 'debug.log.out.txt'
+	LOG_LEVEL = logging.DEBUG
+else:
+	LOG_FILENAME = 'sqlmr.log.out.txt'
+	LOG_LEVEL = logging.INFO
+
+# Set up a specific logger with our desired output level
+logger = logging.getLogger('Tripolium')
+formatter = logging.Formatter("%(name)s,%(levelname)s,%(message)s")
+# formatter = logging.Formatter("%(asctime)s,%(name)s,%(levelname)s,%(message)s")
+
+# Add the log message handler to the logger
+rotFileHandler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=1024, backupCount=1)
+conHandler = logging.StreamHandler()
+
+rotFileHandler.setFormatter(formatter)
+conHandler.setFormatter(formatter)
+
+logger.setLevel(LOG_LEVEL)
+logger.addHandler(rotFileHandler)
+logger.addHandler(conHandler)
+
 		
 		
 class BaseMapper(object):
